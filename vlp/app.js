478a9bce292c18da4332e44a73e27e8884ff729e
siddheshkadam1881 <siddheshkadam111@gmail.com>
2018-06-21 16:27:30 +0530

    working on swagger api

--- vlp/app.js
+++ vlp/app.js
@@ -3,7 +3,9 @@
 var SwaggerConnect = require('swagger-connect');
 var app = require('connect')();
 module.exports = app; // for testing
-
+var connect    = require("./config/db");
+var bodyParser = require('body-parser');
+//var logger=require('./config/logger');
 var config = {
   appRoot: __dirname // required config
 };
@@ -13,9 +15,31 @@ SwaggerConnect.create(config, function(err, swaggerConnect) {
 
   // install middleware
   swaggerConnect.register(app);
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+app.use(function (err,req,res,next) {
+    logger.error(err.stack);
+    res.status(500).send( {
+    err: 'something blew up'
+  });
+})
 
   var port = process.env.PORT || 10010;
   app.listen(port);
-
 
 });
